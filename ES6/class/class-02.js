f1d4cdac4941f8ab5ba2a9bf6cc4ff823798bbae
李佳珂 <jiakeli1226@163.com>
2019-07-24 18:41:16 +0800

    class

--- ES6/class/class-02.js
+++ ES6/class/class-02.js
@@ -69,11 +69,15 @@ console.log(Object.getPrototypeOf(Child) === Parents)   // true
 
 /* ----------------------------- 类的继承链 -----------------------------
     Class 作为构造函数的语法糖，同时有prototype属性和__proto__属性，因此同时存在两条继承链。
+        +   __proto__ 属性表示构造函数的继承；
+        +   prototype属性的__proto__属性,表示方法的继承
 
     （1）子类的__proto__属性，表示构造函数的继承，总是指向父类。
     （2）子类prototype属性的__proto__属性，表示方法的继承，总是指向父类的prototype属性。
 */
 console.log(Child.__proto__ === Parents)    // true
+console.log(Parents.__proto__ === Function.prototype)   // true
+
 console.log(Child.prototype.__proto__ === Parents.prototype)    // true
 
 console.groupEnd()
