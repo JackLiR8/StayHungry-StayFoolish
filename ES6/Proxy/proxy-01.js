4d9885dbe5e1a6b6e3adb754a700135431e728b9
李佳珂 <jiakeli1226@163.com>
2019-09-26 10:06:27 +0800

    :bulb: Update: proxy实例方法get()

--- ES6/Proxy/proxy-01.js
+++ ES6/Proxy/proxy-01.js
@@ -1,6 +1,7 @@
 +(() => {
 /**
  * @file Proxy
+ * get()
  */
 
   /*  Proxy 用于修改某些操作的默认行为，等同于在语言层面做出修改，所以属于
@@ -41,68 +42,121 @@
       proxy 实例本身（严格地说，是操作行为所针对的对象），其中最后一个参数可选。
   */
 
-  const person = {
-    name: '张三'
-  }
-
-  let proxyPerson = new Proxy(person, {
-    get(target, property) {
-      if (property in target) {
-        return target[property]
-      } else {
-        throw new ReferenceError(`Property/${property} does not exist`)
-      }
+    const person = {
+      name: '张三'
     }
-  })
 
-  console.log(person.height)  // undefined
-  // console.log(proxyPerson.height) // ReferenceError: Property/height does not exist
+    let proxyPerson = new Proxy(person, {
+      get(target, property) {
+        if (property in target) {
+          return target[property]
+        } else {
+          throw new ReferenceError(`Property/${property} does not exist`)
+        }
+      }
+    })
+
+    console.log(person.height)  // undefined
+    // console.log(proxyPerson.height) // ReferenceError: Property/height does not exist
 
   /* get方法可以继承。*/
 
-  let proto = new Proxy({}, {
-    get(target, property) {
-      console.log(`GET ${property}`)
-      return target[property]
-    }
-  })
+    let proto = new Proxy({}, {
+      get(target, property) {
+        console.log(`GET ${property}`)
+        return target[property]
+      }
+    })
 
-  let getInheritance = Object.create(proto)
-  getInheritance.foo
-  // GET foo
+    let getInheritance = Object.create(proto)
+    getInheritance.foo
+    // GET foo
 
   // ------------------- 例：利用get拦截，实现一个生成各种 DOM 节点的通用函数dom -------------------
-  const dom = new Proxy({}, {
-    get(target, property) {
-      return function (attrs = {}, ...children) {
-        const el = document.createElement(property);
+    const dom = new Proxy({}, {
+      get(target, property) {
+        return function (attrs = {}, ...children) {
+          const el = document.createElement(property);
 
-        for (const prop of Object.keys(attrs)) {
-          el.setAttribute(prop, attrs[prop])
-        }
-
-        for (let child of children) {
-          if (typeof child === 'string') {
-            child = document.createTextNode(child);
+          for (const prop of Object.keys(attrs)) {
+            el.setAttribute(prop, attrs[prop])
           }
-          el.appendChild(child)
-        }
 
-        return el;
+          for (let child of children) {
+            if (typeof child === 'string') {
+              child = document.createTextNode(child);
+            }
+            el.appendChild(child)
+          }
+
+          return el;
+        }
+      }
+    });
+
+    const el = dom.div({},
+      'Hello, my name is ',
+      dom.a({href: '123'}, 'Jack'),
+      '. I like:',
+      dom.ul({},
+        dom.li({}, 'Basketball'),
+        dom.li({}, 'Football'),
+        dom.li({}, 'Guitar')
+      )
+    )
+
+    document.body.appendChild(el);
+  
+  // 若一个属性不可配置且不可写， 则Proxy不能修改该属性，通过Proxy对象访问该属性会报错
+    const targetUnconf = Object.defineProperty({}, 'foo', {
+      value: 123,
+      writable: false,
+      configurable: false
+    })
+
+    const proxyUnconf = new Proxy(targetUnconf, {
+      get(target, propertyKey) {
+        return 'hello'
+      }
+    }) 
+
+    try {
+      console.log(proxyUnconf.foo)
+    } catch (err) {
+      console.log(err)
+      /*  'get' on proxy: property 'foo' is a read-only and non-configurable data
+       property on the proxy target but the proxy did not return its actual value 
+       (expected '123' but got 'hello') */
+    }
+
+    // ------- 下面的例子通过proxy, 把地址由 http 转为 https
+    let infoData = [
+      {
+        address: 'http://123.com',
+        name: 'haha'
+      }, {
+        address: 'http://456.com',
+        name: 'dudu'
+      }, {
+        address: 'http://789.com',
+        name: 'lele'
+      }
+    ]
+
+    const handler = {
+      get(target, propKey) {
+        if (propKey === 'address') {
+          let a = target[propKey].split('//')[1]
+          return `https://${a}`
+        } 
+        return target[propKey]
       }
     }
-  });
 
-  const el = dom.div({},
-    'Hello, my name is ',
-    dom.a({href: '123'}, 'Jack.'),
-    ' I like:',
-    dom.ul({},
-      dom.li({}, 'Basketball'),
-      dom.li({}, 'Football'),
-      dom.li({}, 'Guitar')
-    )
-  )
+    const infoProxy = infoData.map(v => new Proxy(v, handler))
 
-  document.body.appendChild(el);
+    for (const v of infoProxy) {
+      console.log(v.address)
+    }
+
 })()
