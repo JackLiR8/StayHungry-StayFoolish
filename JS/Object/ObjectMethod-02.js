a0113ed3cf877fab9343a3a58ed2db56e8415753
JackLiR8 <jackli1024@gmail.com>
2019-09-27 14:07:36 +0800

    :bulb: Update: 新增Object.seal(), Object.freeze(), Object.getOwnPropertyNames()

--- JS/Object/ObjectMethod-02.js
+++ JS/Object/ObjectMethod-02.js
@@ -4,6 +4,9 @@
  * Object.getPrototypeOf()
  * Object.is() 
  * Object.isExtensible()
+ * Object.seal()
+ * Object.freeze()
+ * Object.getOwnPropertyNames()
  */
 
 +(() => {
@@ -97,4 +100,106 @@
     // 冻结对象也是不可扩展.
     var frozen = Object.freeze({});
     Object.isExtensible(frozen); // === false
+
+/* --------------------- Object.seal() ---------------------
+  Object.seal()方法封闭一个对象，阻止添加新属性并将所有现有属性标记为不可配置。
+  当前属性的值只要可写就可以改变。
+
+    Object.seal(obj)
+
+  通常，一个对象是可扩展的（可以添加新的属性）。密封一个对象会让这个对象变的不能
+  添加新属性，且所有已有属性会变的不可配置。属性不可配置的效果就是属性变的不可删除，
+  以及一个数据属性不能被重新定义成为访问器属性，或者反之。但属性的值仍然可以修改。
+  尝试删除一个密封对象的属性或者将某个密封对象的属性从数据属性转换成访问器属性，
+  结果会静默失败或抛出TypeError（在严格模式 中最常见的，但不唯一）。
+*/
+  const sealObj = {
+    prop1: 42
+  }
+
+  Object.seal(sealObj);
+
+  sealObj.prop1 = 30;
+  console.log(sealObj.prop1); // 30
+
+  delete sealObj.prop1
+  console.log(sealObj.prop1)  // 30 删除无效
+
+  sealObj.prop2 = 40;
+  console.log(sealObj)    // {prop1: 30}  密封对象添加无效
+
+/* --------------------- Object.freeze() ---------------------
+
+  Object.freeze() 方法可以冻结一个对象。一个被冻结的对象再也不能被修改；冻结了一个对象则不能
+  向这个对象添加新的属性，不能删除已有属性，不能修改该对象已有属性的可枚举性、可配置性、可写性，
+  以及不能修改已有属性的值。此外，冻结一个对象后该对象的原型也不能被修改。freeze() 返回和传入
+  的参数相同的对象。
+
+    Object.freeze(obj)
+*/
+
+  // 浅冻结 
+  // 被冻结的对象是不可变的。但也不总是这样。下例展示了冻结对象不是常量对象（浅冻结）。
+  let obj1 = {
+    internal: {}
+  };
+  
+  Object.freeze(obj1);
+  obj1.internal.a = 'aValue';
+  
+  console.log(obj1.internal.a) // 'aValue'
+
+  // 深冻结函数
+  function deepFreeze(obj) {
+    const propsNames = Object.getOwnPropertyNames(obj);
+
+    for (const v of propsNames) {
+      let prop = obj[v];
+
+      if (typeof prop === 'object' && prop !== null) {
+        deepFreeze(prop)
+      }
+    }
+
+    return Object.freeze(obj)
+  }
+
+  let deep = {
+    a: {
+      b: '123',
+    }
+  }
+
+  deepFreeze(deep);
+
+  deep.a.e = '123';
+  console.log(deep.a.e)   // undefined
+
+  deep.a.b = '456';
+  console.log(deep.a.b)   // '123'
+
+/* --------------------- Object.getOwnPropertyNames() ---------------------
+  Object.getOwnPropertyNames()方法返回一个由指定对象的所有自身属性的属性名（包括不可枚举
+  属性但不包括Symbol值作为名称的属性）组成的数组。
+
+    Object.getOwnPropertyNames(obj)
+*/
+
+  let demo = {
+    a: 1
+  }
+
+  let demo1 = Object.defineProperties(demo, {
+    b: {
+      value: 2
+    },
+    _c: {
+      value: 3,
+      enumerable: true
+    }
+  })
+
+  console.log(Object.getOwnPropertyNames(demo1)); // ["a", "b", "_c"]
+  console.log(Object.keys(demo1));  // ["a", "_c"]
+
 })()
