f6a8fe01d973fd4ff5766ad05ec13c8833cb92bb
JackLiR8 <jiakeli1024@gmail.com>
2019-10-09 18:42:49 +0800

    :bulb: Update: proxy实现观察者模式

--- ES6/Proxy/observer.js
+++ ES6/Proxy/observer.js
@@ -2,16 +2,42 @@
  * @file Proxy 实现观察者模式
  */
 +(() => {
+  // 观察者队列
+  const queuedObservers = new Set();
+
+  /**
+   * 
+   * @param  {...Function} fn 
+   */
+  const observe = (...fn) => {
+    fn.forEach(v => {
+      queuedObservers.add(v)
+    })
+  };
+  const observable = obj => new Proxy(obj, {set});
+
+  function set(target, key, value, receiver) {
+    const result = Reflect.set(target, key, value, receiver);
+    queuedObservers.forEach(observe => observe());
+    return result;
+  }
+
   const person = observable({
     name: 'Jake',
     age: 10
   });
 
   function print() {
-    console.log(`资料变更：name: ${person.name}, age: ${person.age}`)
+    console.log(`name: ${person.name}, age: ${person.age}`)
   }
 
-  observe(print);
+  function alertChange() {
+    console.log('data is changing')
+  }
+
+  observe(alertChange, print);
   person.name = 'Kobe';
-  // 资料变更：name: kobe, age: 10
-})
+  // data is changing
+  // name: Kobe, age: 10
+
+})()
