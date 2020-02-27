3c1a9d70513b15903782cc597113dcc536aa2dd8
JackLiR8 <jiakeli1024@gmail.com>
2020-02-27 21:12:34 +0800

    :art: leetcode-1 优化

--- Leetcode/q-01.js
+++ Leetcode/q-01.js
@@ -6,19 +6,31 @@
  */
 
 const twoSum = function(target, nums) {
+  let map = new Map()
+
+  for (let i = 0; i < nums.length; i++) {
+    const dif = target - nums[i]
+    if (map.has(dif)) {
+      return [map.get(dif), i]
+    }
+    map.set(nums[i], i)
+  }
+  return []
+}
+/* const twoSum = function(target, nums) {
   let _nums = [...nums]
 
   for (let i = 0; i < nums.length; i++) {
+    _nums.shift()
     const num = nums[i]
     const tem = target - num
-    const j = _nums.slice(1).findIndex(v => v === tem)
+    const j = _nums.indexOf(tem)
     if (j > -1) {
       return [i, i + j + 1] 
-    } else {
-      _nums.shift()
     }
   }
-}
+  return []
+} */
 
 const target = 6
 const nums = [3, 2, 4]
