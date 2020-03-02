27a2db5593e46014289f67d7c2b25d977ff6e1fc
JackLiR8 <jiakeli1024@gmail.com>
2020-03-02 16:31:09 +0800

    :construction: LeetCode q-2

--- Leetcode/001_100/q-02.js
+++ Leetcode/001_100/q-02.js
@@ -36,7 +36,7 @@ function addTwoNumbers(l1, l2) {
   while (p || q ) {
     let num1 = p && p.val || 0
     let num2 = q && q.val || 0
-    sum = num1 + num2 + carry
+    let sum = num1 + num2 + carry
 
     let newNode = new ListNode(sum % 10)
     curr.next = newNode
@@ -57,3 +57,59 @@ function addTwoNumbers(l1, l2) {
 
 const res = addTwoNumbers(l1, l2)
 console.log('res', res)
+
+/**
+ * 此中方法适用于两个链表长度相差较大时的情况
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+function addTwoNumbers1(l1, l2) {
+  let dummyHead = new ListNode(0)
+  let [p, q, carry] = [l1, l2, 0]
+
+  // curr 记录当前结点
+  let curr = dummyHead
+  while (p || q || carry) {
+    if (p !== null && q === null) {
+      if (carry) {
+        if (p.val < 9) {
+          let _node = new ListNode(p.val + carry)
+          _node.next = p.next
+          curr.next = _node
+        }
+      } else {
+        curr.next = p
+        break
+      }
+    } else if (p === null && q !== null) {
+      if (carry) {
+        if (q.val < 9) {
+          let _node = new ListNode(q.val + carry)
+          _node.next = q.next
+          curr.next = _node
+        }
+      } else {
+        curr.next = q
+        break
+      }
+    }
+
+    let num1 = p && p.val || 0
+    let num2 = q && q.val || 0
+    let sum = num1 + num2 + carry
+
+    let newNode = new ListNode(sum % 10)
+    curr.next = newNode
+    curr = newNode
+    p = p ? p.next : null
+    q = q ? q.next : null
+    
+    carry = Math.floor(sum / 10)
+  }
+
+  return dummyHead.next
+}
+
+const res1 = addTwoNumbers1(l1, l2)
+console.log('res1', res1)
